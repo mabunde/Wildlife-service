cd8231b21d4000eb992aae4e148ce8a6928e0baf
mabunde <solomonsitati39@gmail.com>
2021-11-09 08:36:43 +0300

    save and retrieve animal name from database

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,28 @@
-import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class AnimalTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void animals_instantiatesCorrectly_true() {
+        Animal wild = new Animal("Rhino");
+        assertTrue(wild instanceof Animal);
+    }
+    @Test
+    public void equals_returnsTrueIfNameAndIdAreEqual() {
+        Animal wild = new Animal("Rhino");
+        Animal tame = new Animal("Rhino");
+        assertTrue(wild.equals(tame));
+    }
+    @Test
+    public void save_insertsObjectIntoDatabase_Animal() {
+        Animal wild = new Animal("Lucky");
+        wild.save();
+        assertTrue(Animal.all().get(0).equals(wild));
+    }
 
 }
