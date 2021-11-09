c2283477020915a2d1050e47b54ebcf0dffa5111
mabunde <solomonsitati39@gmail.com>
2021-11-09 12:49:06 +0300

    Update sql file

--- create.sql
+++ create.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE wildlife_tracker;
-CREATE TABLE animals();
+CREATE TABLE animals (id serial PRIMARY KEY, name VARCHAR, endangered VARCHAR, health VARCHAR, age INT, location VARCHAR);
+
+CREATE TABLE sightings (id serial PRIMARY KEY, animalId int, location VARCHAR, rangername VARCHAR, lastsighting timestamp);
 CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
